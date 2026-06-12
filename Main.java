3deb93ebb8c786a1873f68bdff601a190119b47f
gopika <gopikag.info@gmail.com>
2026-06-12 22:54:33 +0530

    Add custom bubble sort logic

--- Main.java
+++ Main.java
@@ -11,4 +11,18 @@ public class Main {
             numbers[i] = sc.nextInt();
         }
     }
+// Custom Bubble Sort algorithm
+    public static void sortArray(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap elements using a temporary variable
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
 }
