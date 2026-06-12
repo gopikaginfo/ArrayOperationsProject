5ef025f8855ac7125d4722ad66fd85776eb7d876
gopika <gopikag.info@gmail.com>
2026-06-12 22:58:28 +0530

    Final application workflow complete: calling methods in main

--- Main.java
+++ Main.java
@@ -10,8 +10,13 @@ public class Main {
             System.out.print("Number " + (i + 1) + ": ");
             numbers[i] = sc.nextInt();
         }
+        
+        // --- ADD THESE TWO LINES TO RUN THE LOGIC ---
+        sortArray(numbers);
+        findElements(numbers);
     }
-// Custom Bubble Sort algorithm
+
+    // Custom Bubble Sort algorithm
     public static void sortArray(int[] arr) {
         int n = arr.length;
         for (int i = 0; i < n - 1; i++) {
@@ -24,5 +29,15 @@ public class Main {
                 }
             }
         }
+    }
+
+    // Method to extract elements assuming the array is already sorted
+    public static void findElements(int[] arr) {
+        int secondLowest = arr[1];
+        int secondHighest = arr[arr.length - 2];
+
+        System.out.println("\n--- Results ---");
+        System.out.println("Second Lowest: " + secondLowest);
+        System.out.println("Second Highest: " + secondHighest);
     }
 }
