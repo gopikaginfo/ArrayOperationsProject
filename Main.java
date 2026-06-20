13cfa931b3208a03e7afb40d8e5c22fd1e6c7d1f
gopika <gopikag.info@gmail.com>
2026-06-20 10:50:26 +0530

    Refactor code to use OOP methods matching mentor style

--- Main.java
+++ Main.java
@@ -1,43 +1,52 @@
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int[] numbers = new int[5];
+class Main {
+    // Data member
+    int[] numbers;
 
+    // Default Constructor
+    Main() {
+        this.numbers = new int[5];
+    }
+
+    // Method to handle input
+    void acceptInput() {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter 5 different numbers:");
         for (int i = 0; i < 5; i++) {
             System.out.print("Number " + (i + 1) + ": ");
-            numbers[i] = sc.nextInt();
+            this.numbers[i] = sc.nextInt();
         }
-        
-        // --- ADD THESE TWO LINES TO RUN THE LOGIC ---
-        sortArray(numbers);
-        findElements(numbers);
     }
 
-    // Custom Bubble Sort algorithm
-    public static void sortArray(int[] arr) {
-        int n = arr.length;
+    // Method dedicated to sorting logic (Bubble Sort)
+    void sortArray() {
+        int n = this.numbers.length;
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    // Swap elements using a temporary variable
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+                if (this.numbers[j] > this.numbers[j + 1]) {
+                    int temp = this.numbers[j];
+                    this.numbers[j] = this.numbers[j + 1];
+                    this.numbers[j + 1] = temp;
                 }
             }
         }
     }
 
-    // Method to extract elements assuming the array is already sorted
-    public static void findElements(int[] arr) {
-        int secondLowest = arr[1];
-        int secondHighest = arr[arr.length - 2];
+    // Method to handle metrics display
+    void displayMetrics() {
+        int secondLowest = this.numbers[1];
+        int secondHighest = this.numbers[this.numbers.length - 2];
 
         System.out.println("\n--- Results ---");
         System.out.println("Second Lowest: " + secondLowest);
         System.out.println("Second Highest: " + secondHighest);
+    }
+
+    public static void main(String[] args) {
+        Main arrayObj = new Main();
+        arrayObj.acceptInput();
+        arrayObj.sortArray();
+        arrayObj.displayMetrics();
     }
 }
